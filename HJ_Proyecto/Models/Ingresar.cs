4fd13b008cfc6d92d5e207ea1aec022855e04b45
Hernan <hernan.jurado@udla.edu.ec>
2023-10-31 21:59:40 -0500

    Pruebas ingresar

--- HJ_Proyecto/Models/Ingresar.cs
+++ HJ_Proyecto/Models/Ingresar.cs
@@ -2,5 +2,14 @@
 {
     public class Ingresar
     {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+ 
+        public string? Email { get; set; }
+
     }
 }
+
